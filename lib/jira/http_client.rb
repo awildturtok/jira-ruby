45bddc7903606845cc2862aaefab105fd3afe3b2
Fabian Kovacs <fixed-term.fabian.kovacs@bosch-si.com>
2017-12-07 15:09:54 +0100

    added reading proxy from env

--- lib/jira/http_client.rb
+++ lib/jira/http_client.rb
@@ -46,7 +46,9 @@ module JIRA
       if @options[:proxy_address]
         proxy_uri = URI.parse(@options[:proxy_address])
         http_class = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
-        
+      elsif ENV["http_proxy"]
+        proxy_uri = URI.parse(ENV[:proxy_address])
+        http_class = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
       else
         http_class = Net::HTTP
       end
